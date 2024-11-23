4da5277cde66cb847736aa40a7125be82418536f
charliegood2046 <149835864+charliegood2046@users.noreply.github.com>
2024-11-23 17:26:05 -0600

    Bulleted synonyms.

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   return (
@@ -7,7 +8,9 @@ export default function Meaning(props) {
 
       <div key={props.index}>
         <p>
-          {props.meaning.definition}
+          <strong>Definition:</strong> {props.meaning.definition}
+          <br />
+          <Synonyms synonyms={props.meaning.synonyms} />
           <br />
           <em>{props.meaning.example}</em>
         </p>
