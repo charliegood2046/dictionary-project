723126cbd292e06dd53ac454a9d7f492f5bb3820
charliegood2046 <149835864+charliegood2046@users.noreply.github.com>
2024-11-22 20:41:56 -0600

    Add Axios & SheCodes Dict API.

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,22 @@
 import React, { useState } from "react";
 import "./Dictionary.css";
+import axios from "axios";
 
 export default function Dictionary() {
   const [keyword, setKeyword] = useState("");
 
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
   function search(event) {
     event.preventDefault();
     alert(`Defining ${keyword}`);
+
+    let apiKey = "f675a384aa40tc5a0420348fddabad0o";
+
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
